5b302686c14d4aa00ca7c4d07482a15f5fc1f860
jaeseung-lee-engineer <jaeseung.lee124@gmail.com>
2026-07-23 12:12:19 -0400

    1212

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,9 @@ export const metadata: Metadata = {
     title: "New York UBF",
   },
   icons: {
-    icon: "/icon-192-v2.png",
-    apple: "/apple-touch-icon-v2.png",
-    shortcut: "/icon-192-v2.png",
+    icon: "/icon-192-v3.png",
+    apple: "/apple-touch-icon-v3.png",
+    shortcut: "/icon-192-v3.png",
   },
 };
 
